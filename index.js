d3fd1255c9ab98d4a6ea0544af0c2e56f42a08ba
Boggy <boggy@boggy.ninja>
2021-11-04 21:19:35 +0100

    Changed alerts into cyan messages. It's stylish now :)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var title = document.getElementById('mainlogothing');
+var msg = document.getElementById('messagething');
 var currenttitle = "boggy"
 
 //get keybord event listener
@@ -9,18 +10,25 @@ document.addEventListener('keydown', function(event) {
     if((key >= 48 && key <= 57) || (key >= 65 && key <= 90)) {
         currenttitle = currenttitle.substr(1, 4) + event.key;
         title.innerHTML = "> " + currenttitle;
-        if (currenttitle.toLowerCase() == "boggy") {
-            alert("Hey, that's me!");
+        if(msg != null) {
+            if (currenttitle.toLowerCase() == "boggy") {
+                msg.innerHTML = "Hey, that's me!";
+                msg.style.color = "aqua";
+            }
+            else if (currenttitle.toLowerCase() == "hello") {
+                msg.innerHTML = "Hello, World!";
+                msg.style.color = "aqua";
+            }
+            else if (currenttitle.toLowerCase() == "plaao") {
+                msg.innerHTML = "A very cool person.";
+                msg.style.color = "aqua";
+            }
+            else if (currenttitle.toLowerCase() == "vaske") {
+                msg.innerHTML = "Odrer has been rsetored.";
+                msg.style.color = "aqua";
+            }
         }
-        else if (currenttitle.toLowerCase() == "hello") {
-            alert("Hello, World!");
-        }
-        else if (currenttitle.toLowerCase() == "plaao") {
-            alert("A very cool person.");
-        }
-        else if (currenttitle.toLowerCase() == "vaske") {
-            alert("Odrer has been rsetored.");
-        }
+        
     }
 })
 
