cd35c97e96bf580542bb033e8c80f8a96f206dbb
Boggy <bogdan.micic@elfak.rs>
2023-10-26 17:45:56 +0200

    web1

---
+++ ctf/web1/index.js
@@ -0,0 +1,4 @@
+function clicked()
+{
+    var xzxzx=document.getElementById("\x69\x6E\x70\x75\x74").value;var divvy=document.getElementById("\x64\x69\x76\x76\x79");if(xzxzx=="pr" + (1+1+1+1-1).toString() + "s4n_l" + (1+1-1+1+1).toString() + "b4c_" + (1).toString() + "_b" + (1-1+1+1+1).toString() + "lmuz"){alert("\x4E\x41\x49\x5B\x73\x74\x61\x5F\x74\x30\x5F\x72\x61\x64\x69\x73\x5F\x75\x5F\x6A\x73\x5D")}else {if(input== ""){return}else {divvy.innerHTML+= xzxzx+ "<br>"}}
+}
